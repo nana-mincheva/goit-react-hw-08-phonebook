788855255102dc330e705a4bdc04303faa67e825
Nadya Mincheva <7000585m1@gmail.com>
2023-03-01 18:13:17 +0200

    Updates

--- static/js/828.5d21fb62.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[828],{5828:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(5545),c="Home_container__w+u63",_="Home_text__YlQ2v",a=o(2559),r=function(){return(0,a.jsxs)("main",{className:c,children:[(0,a.jsx)("p",{className:_,children:"Your contact book"}),(0,a.jsx)(t.j3,{})]})}}}]);
-//# sourceMappingURL=828.5d21fb62.chunk.js.map
