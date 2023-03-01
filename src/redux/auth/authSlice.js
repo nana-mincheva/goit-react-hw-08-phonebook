9ff7c0b94b60d9d4eb3363b65db4e1d2f49904b7
Nadya Mincheva <7000585m1@gmail.com>
2023-03-01 18:10:46 +0200

    hw8

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -94,10 +94,17 @@ const AuthSlice = createSlice({
     [authOperation.logIn.rejected](state, { payload }) {
       state.error = `${payload}`;
     },
+    [authOperation.logOut.pending](state) {
+      state.isLoggingOut = true;
+    },
     [authOperation.logOut.fulfilled](state, _) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isloggedIn = false;
+    },
+    [authOperation.logOut.rejected](state, action) {
+      state.error = action.payload;
+      state.isLoggingOut = false;
     },
       [authOperation.fetchCurrentUser.pending](state) {
       state.isCurrentUserLoading = true;
